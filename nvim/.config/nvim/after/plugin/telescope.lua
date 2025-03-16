0efea4e22d55b3c12e81c2403af3790bd3e446a3
canozokur <13416785+canozokur@users.noreply.github.com>
2025-03-17 00:48:03 +0200

    Show hidden files with Telescope find files

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,4 @@
 local builtin = require('telescope.builtin')
 local utils = require('telescope.utils')
-vim.keymap.set('n', '<C-o>', function() builtin.find_files({ cwd = utils.buffer_dir() }) end, {})
+vim.keymap.set('n', '<C-o>', function() builtin.find_files({ hidden = true }) end, {})
 vim.keymap.set('n', '<leader>ps', function() builtin.live_grep({ cwd = utils.buffer_dir() }) end, {})
