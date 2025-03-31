0259b6df51283a93c18d79181e6a51ba05c45f2e
canozokur <13416785+canozokur@users.noreply.github.com>
2025-03-31 15:05:12 +0300

    Only ignore .git, not .github

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,4 @@
 local builtin = require('telescope.builtin')
 local utils = require('telescope.utils')
-vim.keymap.set('n', '<C-o>', function() builtin.find_files({ hidden = true, file_ignore_patterns = { 'node_modules', '.git', '.venv' }}) end, {})
+vim.keymap.set('n', '<C-o>', function() builtin.find_files({ hidden = true, file_ignore_patterns = { 'node_modules', '.git/', '.venv' }}) end, {})
 vim.keymap.set('n', '<leader>ps', function() builtin.live_grep({ cwd = utils.buffer_dir() }) end, {})
