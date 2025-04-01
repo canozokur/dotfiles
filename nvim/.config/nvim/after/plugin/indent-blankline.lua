e9ec60dd2b05e6211e382df444cea07fb87c5dbd
canozokur <13416785+canozokur@users.noreply.github.com>
2025-04-01 15:28:18 +0300

    Update listchars

--- nvim/.config/nvim/after/plugin/indent-blankline.lua
+++ nvim/.config/nvim/after/plugin/indent-blankline.lua
@@ -2,9 +2,8 @@ require("ibl").setup()
 
 vim.opt.list = true
 vim.opt.listchars = {
-  eol   = '$',
-  lead  = '␣',
-  trail = '␣',
-  tab   = '>-',
-  nbsp  = '␣',
+  multispace = '·',
+  eol        = '↲',
+  tab        = '→ ',
+  trail      = '·',
 }
