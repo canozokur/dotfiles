10b69af916a3899652a0bc64c8ba2a8a561bbd72
canozokur <13416785+canozokur@users.noreply.github.com>
2025-05-13 16:24:13 +0300

    Add git root func

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,27 @@
 local builtin = require('telescope.builtin')
-local utils = require('telescope.utils')
+
+local function live_grep_from_project_git_root()
+	local function is_git_repo()
+		vim.fn.system("git rev-parse --is-inside-work-tree")
+
+		return vim.v.shell_error == 0
+	end
+
+	local function get_git_root()
+		local dot_git_path = vim.fn.finddir(".git", ".;")
+		return vim.fn.fnamemodify(dot_git_path, ":h")
+	end
+
+	local opts = {}
+
+	if is_git_repo() then
+		opts = {
+			cwd = get_git_root(),
+		}
+	end
+
+	require("telescope.builtin").live_grep(opts)
+end
+
 vim.keymap.set('n', '<C-o>', function() builtin.find_files({ hidden = true, file_ignore_patterns = { 'node_modules', '.git/', '.venv' }}) end, {})
-vim.keymap.set('n', '<leader>ps', function() builtin.live_grep({ cwd = utils.buffer_dir() }) end, {})
+vim.keymap.set('n', '<leader>ps', function() live_grep_from_project_git_root() end, {})
