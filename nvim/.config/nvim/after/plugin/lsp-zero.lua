11ca590061b715d75ce8b0f967e3e8fd1939f413
canozokur <13416785+canozokur@users.noreply.github.com>
2025-05-14 15:11:38 +0300

    Use telescope instead of builtin pickers

--- nvim/.config/nvim/after/plugin/lsp-zero.lua
+++ nvim/.config/nvim/after/plugin/lsp-zero.lua
@@ -4,9 +4,8 @@ lsp_zero.on_attach(function(client, bufnr)
   local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+  vim.keymap.set("n", "gr", '<cmd>Telescope lsp_references<cr>', opts)
+  vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<cr>', opts)
   vim.keymap.set("n", "<leader>gn", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set({ 'n', 'x' }, '=', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
