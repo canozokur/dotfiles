7f16512d575d4b14d91e27279c706743c6ebbc5f
canozokur <13416785+canozokur@users.noreply.github.com>
2025-05-14 15:12:19 +0300

    Add to quickfix list in insert mode instead of overwriting it

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,6 @@
+local telescope = require('telescope')
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
 
 local function live_grep_from_project_git_root()
 	local function is_git_repo()
@@ -22,6 +24,16 @@ local function live_grep_from_project_git_root()
 
 	require("telescope.builtin").live_grep(opts)
 end
+
+telescope.setup({
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-q>"] = actions.add_to_qflist + actions.open_qflist,
+      },
+    },
+  },
+})
 
 vim.keymap.set('n', '<C-o>', function() builtin.find_files({ hidden = true, file_ignore_patterns = { 'node_modules', '.git/', '.venv' }}) end, {})
 vim.keymap.set('n', '<leader>ps', function() live_grep_from_project_git_root() end, {})
